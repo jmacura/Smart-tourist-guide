9ad69157d7b7b4f9acfebc92ebc68649610ed372
jmacura <macurajan@gmail.com>
2017-02-16 14:33:21 +0100

    authorship update & whitespace refactoring

--- skript.js
+++ skript.js
@@ -3,6 +3,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 // @author: jmacura 2016-2017
+// @author: kellar 2017
 
 // global variables
 var destination = null;
