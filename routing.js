95004bc28cf6cfaf8351fce814d1372c51701032
jmacura <macurajan@gmail.com>
2017-02-20 00:12:04 +0100

    Bugfixes, small improvements Fixed broken export Improved KML export (file name and description) Added more posibilities for point colours in map

--- routing.js
+++ routing.js
@@ -1,2 +1,3 @@
 // JavaScript Document
-'http://router.project-osrm.org/route/v1/driving/13.388860,52.517037;13.397634,52.529407;13.428555,52.523219?overview=false'
+//'http://router.project-osrm.org/route/v1/driving/13.388860,52.517037;13.397634,52.529407;13.428555,52.523219?overview=false'
+//TODO
